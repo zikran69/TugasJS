739069bff3ac91507292eb5c6bf80f28ddf5edf8
zikran69 <zikzigi69@gmail.com>
2023-09-06 00:11:06 +0700

    menyelesaikan tugas js

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,36 @@ const showHiddenPass = (loginPass, loginEye) =>{
 
 showHiddenPass('login-pass','login-eye')
 
+const user = {
+   username: 'Ziki',
+   password: '123',
+}
+const rand1 = Math.floor(Math.random() * 100) + 1
+const rand2 = Math.floor(Math.random() * 10) + 1
+
+const jw1 = document.getElementById('jw1')
+jw1.innerHTML='<input type="text" value="'+rand1+'+'+rand2+'" class="mt-1 px-4 text-center py-2 w-full border rounded-md focus:ring focus:ring-indigo-300"/>'
+const jawaban =rand1+rand2
+btnLoginElm.addEventListener('click', function () {
+    const username = usernameElm.value
+    const password = passwordElm.value
+    const jawabannya = jawabElm.value
+
+    if (username !== "" && password !== "") {
+        if ((username.toLowerCase() === user.username.toLowerCase()) && (password === user.password)) {
+            if(jawabannya == jawaban){
+                alert('Anda Berhasil Login')
+                landingElm.style.display = 'none'
+               
+            } else {
+                alert('Gagal Login, Jawaban Anda Salah')
+            }            
+
+        } else {
+            alert('Username dan Password anda salah')
+        }
+    } else {
+        alert('Username dan Password harus diisi dulu')
+    }
+
+})
